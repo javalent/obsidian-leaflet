d6e70d2eb60f426a2f7be224605ede24da53a97d
valentine195 <38669521+valentine195@users.noreply.github.com>
2021-03-05 12:11:38 -0500

    Update rollup.config-build.js

--- rollup.config-build.js
+++ rollup.config-build.js
@@ -1,35 +1,3 @@
-<<<<<<< Updated upstream
-import typescript from "@rollup/plugin-typescript";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import copy from "rollup-plugin-copy";
-
-export default {
-	input: "./src/main.ts",
-	output: {
-		dir:
-			"C:/Users/jvalentine/Documents/GitHub/Personal/obsidian-leaflet-plugin",
-		sourcemap: "inline",
-		format: "cjs",
-		exports: "default",
-	},
-	external: ["obsidian"],
-	plugins: [
-		typescript(),
-		nodeResolve({ browser: true }),
-		commonjs(),
-		copy({
-			targets: [
-				{
-					src: ["./*", '!./node_modules'],
-					dest:
-						"C:/Users/jvalentine/Documents/GitHub/Personal/obsidian-leaflet-plugin/",
-				},
-			],
-		}),
-	],
-};
-=======
 import typescript from '@rollup/plugin-typescript';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
@@ -49,4 +17,3 @@ export default {
     commonjs(),
   ]
 };
->>>>>>> Stashed changes
