a0a058f44290af6d2c18a3b27accd391aac83777
valentine195 <valentine.195@gmail.com>
2021-08-07 12:49:16 -0400

    moved unload handler to onunload

--- src/leaflet.ts
+++ src/leaflet.ts
@@ -88,7 +88,7 @@ export class LeafletRenderer extends MarkdownRenderChild {
     parentEl: HTMLElement;
     constructor(
         private plugin: ObsidianLeaflet,
-        ctx: MarkdownPostProcessorContext,
+        private ctx: MarkdownPostProcessorContext,
         container: HTMLElement,
         options: LeafletMapOptions = {}
     ) {
@@ -99,44 +99,6 @@ export class LeafletRenderer extends MarkdownRenderChild {
         this.containerEl.style.height = options.height;
         this.containerEl.style.width = "100%";
         this.containerEl.style.backgroundColor = "var(--background-secondary)";
-
-        this.register(async () => {
-            try {
-                this.map.remove();
-            } catch (e) {}
-
-            let file = this.plugin.app.vault.getAbstractFileByPath(
-                ctx.sourcePath
-            );
-            if (!file || !(file instanceof TFile)) {
-                return;
-            }
-            let fileContent = await this.plugin.app.vault.read(file);
-
-            let containsThisMap: boolean = false,
-                r = new RegExp(
-                    `\`\`\`leaflet[\\s\\S]*?\\bid:(\\s?${this.map.id})\\b\\s*\\n[\\s\\S]*?\`\`\``,
-                    "g"
-                );
-            containsThisMap = fileContent.match(r)?.length > 0 || false;
-
-            if (!containsThisMap) {
-                //Block was deleted or id was changed
-
-                let mapFile = this.plugin.mapFiles.find(
-                    ({ file: f }) => f === ctx.sourcePath
-                );
-                mapFile.maps = mapFile.maps.filter(
-                    (mapId) => mapId != this.map.id
-                );
-            }
-
-            await this.plugin.saveSettings();
-
-            this.plugin.maps = this.plugin.maps.filter((m) => {
-                return m.map != this.map;
-            });
-        });
 
         this.parentEl = ctx.containerEl;
     }
@@ -174,6 +136,43 @@ export class LeafletRenderer extends MarkdownRenderChild {
                 subtree: false
             });
         }
+    }
+
+    async onunload() {
+        super.onunload();
+        try {
+            this.map.remove();
+        } catch (e) {}
+
+        let file = this.plugin.app.vault.getAbstractFileByPath(
+            this.ctx.sourcePath
+        );
+        if (!file || !(file instanceof TFile)) {
+            return;
+        }
+        let fileContent = await this.plugin.app.vault.read(file);
+
+        let containsThisMap: boolean = false,
+            r = new RegExp(
+                `\`\`\`leaflet[\\s\\S]*?\\bid:(\\s?${this.map.id})\\b\\s*\\n[\\s\\S]*?\`\`\``,
+                "g"
+            );
+        containsThisMap = fileContent.match(r)?.length > 0 || false;
+
+        if (!containsThisMap) {
+            //Block was deleted or id was changed
+
+            let mapFile = this.plugin.mapFiles.find(
+                ({ file: f }) => f === this.ctx.sourcePath
+            );
+            mapFile.maps = mapFile.maps.filter((mapId) => mapId != this.map.id);
+        }
+
+        await this.plugin.saveSettings();
+
+        this.plugin.maps = this.plugin.maps.filter((m) => {
+            return m.map != this.map;
+        });
     }
 }
 
