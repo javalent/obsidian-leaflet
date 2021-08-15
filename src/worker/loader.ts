9a1985986b64aeff4ee37c481aae3e1da0ee15e0
valentine195 <valentine.195@gmail.com>
2021-08-15 10:08:18 -0400

    loader implemented

--- src/worker/loader.ts
+++ src/worker/loader.ts
@@ -1,4 +1,4 @@
-import { App, Events, Platform } from "obsidian";
+import { App, Events, Notice, Platform } from "obsidian";
 
 import { getType as lookupMimeType } from "mime/lite";
 
@@ -51,18 +51,22 @@ export default class Loader extends Events {
                     type: "url"
                 });
             } else {
-                console.log("Mobile platform!")
+                console.log("Mobile platform!");
                 layer = {
                     id: blob.id,
                     alias: blob.alias,
-                    ...(await this.toDataURL(blob.blob))
+                    ...(await this.toDataURL(blob.blob)),
+                    h: 0,
+                    w: 0
                 };
                 let mimeType: string;
                 if (blob.extension) {
                     mimeType = lookupMimeType(blob.extension);
                 }
                 layer.data = "data:" + mimeType + layer.data;
-                console.log("🚀 ~ file: loader.ts ~ line 65 ~ layer", layer);
+                const { h, w } = await this.getImageDimensions(layer.data);
+                layer.h = h;
+                layer.w = w;
                 this.trigger(`${id}-layer-data-ready`, layer);
             }
         }
@@ -70,7 +74,7 @@ export default class Loader extends Events {
 
     async toDataURL(
         blob: Blob
-    ): Promise<{ data: string; h: number; w: number }> {
+    ): Promise<{ data: string; }> {
         //determine link type
         return new Promise(async (resolve, reject) => {
             const reader = new FileReader();
@@ -79,17 +83,32 @@ export default class Loader extends Events {
                     let data = reader.result.slice(
                         reader.result.indexOf(";base64,")
                     );
-                    resolve({ data, h, w });
+
+                    resolve({ data });
                 } else {
                     reject();
                 }
             };
-
-            const bitmap = await createImageBitmap(blob);
-            const { height: h, width: w } = bitmap;
+            console.log("🚀 ~ file: loader.ts ~ line 1 ~ blob", blob);
 
             reader.onerror = reject;
             reader.readAsDataURL(blob);
+        });
+    }
+    getImageDimensions(url: string): Promise<{ h: number, w: number }> {
+        return new Promise(function (resolved, reject) {
+            var i = new Image();
+            i.onload = function () {
+                const { width, height } = i;
+                i.detach();
+                resolved({ w: width, h: height });
+            };
+            i.onerror = () => {
+                new Notice("There was an issue getting the image dimensions.");
+                reject();
+            };
+
+            i.src = url;
         });
     }
 }
