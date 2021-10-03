49f85167048a52826779ade25c307315c537dfc0
Jesse Buck <JesseBuck@Outlook.com>
2021-10-03 11:42:01 -0600

    Fix popup removal error preventing marker deletion

--- src/layer/marker.ts
+++ src/layer/marker.ts
@@ -570,7 +570,7 @@ export class Marker extends Layer<DivIconMarker> implements MarkerDefinition {
     remove() {
         this.group && this.group.removeLayer(this.leafletInstance);
         if (this.tooltip == "always") {
-            this.popup.leafletInstance.remove();
+            this.popup.leafletInstance?.remove();
         }
     }
 }
