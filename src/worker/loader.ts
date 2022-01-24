08c80ed73b44ab870d0ae5d4b7947b4690400bec
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2022-01-23 21:43:01 -0500

    feat: much improved image loading

--- src/worker/loader.ts
+++ src/worker/loader.ts
@@ -1,50 +1,26 @@
 import { App, Events, Notice } from "obsidian";
 
-import { getType as lookupMimeType } from "mime/lite";
-
-import { getBlob, getId } from "../utils";
-
-import ImageWorker from "./image.worker";
+import { parseLink } from "../utils";
 import { ImageLayerData } from "src/@types";
 import t from "src/l10n/locale";
 
 export default class Loader extends Events {
-    workers: Map<string, Worker> = new Map();
     constructor(public app: App) {
         super();
     }
     async loadImage(id: string, layers: string[]): Promise<void> {
         for (let image of layers) {
-            let blob = await getBlob(encodeURIComponent(image), this.app),
-                layer: ImageLayerData;
+            const { link, id: layerId, alias } = await this.getLink(image);
 
-            const worker = new ImageWorker();
-            const wId = getId();
-            this.workers.set(wId, worker);
-
-            worker.onmessage = async (event) => {
-                layer = event.data.data;
-
-                layer.id = event.data.id;
-                layer.alias = blob.alias;
-                let mimeType: string;
-                if (blob.extension) {
-                    mimeType = lookupMimeType(blob.extension);
-                }
-                layer.data = "data:" + mimeType + layer.data;
-                const { h, w } = await this.getImageDimensions(layer.data);
-                layer.h = h;
-                layer.w = w;
-                this.trigger(`${id}-layer-data-ready`, layer);
-
-                worker.terminate();
-                this.workers.delete(wId);
+            const { h, w } = await this.getImageDimensions(link);
+            const layer = {
+                data: link,
+                h,
+                w,
+                alias,
+                id: layerId
             };
-
-            worker.postMessage({
-                blobs: [blob],
-                type: "url"
-            });
+            this.trigger(`${id}-layer-data-ready`, layer);
         }
     }
 
@@ -54,68 +30,21 @@ export default class Loader extends Events {
     ): Promise<ImageLayerData> {
         return new Promise(async (resolve, reject) => {
             for (let image of layers) {
-                let blob = await getBlob(encodeURIComponent(image), this.app),
-                    layer: ImageLayerData;
+                const { link, id: layerId, alias } = await this.getLink(image);
 
-                const worker = new ImageWorker();
-
-                const wId = getId();
-                this.workers.set(wId, worker);
-
-                worker.onmessage = async (event) => {
-                    layer = event.data.data;
-
-                    layer.id = event.data.id;
-                    layer.alias = blob.alias;
-                    let mimeType: string;
-                    if (blob.extension) {
-                        mimeType = lookupMimeType(blob.extension);
-                    }
-                    layer.data = "data:" + mimeType + layer.data;
-                    const { h, w } = await this.getImageDimensions(layer.data);
-                    layer.h = h;
-                    layer.w = w;
-
-                    worker.terminate();
-                    this.workers.delete(wId);
-
-                    resolve(layer);
+                const { h, w } = await this.getImageDimensions(link);
+                const layer = {
+                    data: link,
+                    h,
+                    w,
+                    alias,
+                    id: layerId
                 };
-
-                worker.postMessage({
-                    blobs: [blob],
-                    type: "url"
-                });
+                resolve(layer);
             }
         });
     }
-    unload() {
-        for (let [, worker] of this.workers) {
-            worker.terminate();
-        }
-        this.workers = new Map();
-    }
-
-    async toDataURL(blob: Blob): Promise<{ data: string }> {
-        //determine link type
-        return new Promise(async (resolve, reject) => {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                if (typeof reader.result === "string") {
-                    let data = reader.result.slice(
-                        reader.result.indexOf(";base64,")
-                    );
-
-                    resolve({ data });
-                } else {
-                    reject();
-                }
-            };
-
-            reader.onerror = reject;
-            reader.readAsDataURL(blob);
-        });
-    }
+    unload() {}
     getImageDimensions(url: string): Promise<{ h: number; w: number }> {
         return new Promise(function (resolved, reject) {
             var i = new Image();
@@ -133,5 +62,32 @@ export default class Loader extends Events {
 
             i.src = url;
         });
+    }
+    async getLink(url: string) {
+        url = decodeURIComponent(url);
+        let type: "link" | "file";
+        let link: string, alias: string;
+        try {
+            if (/https?:/.test(url)) {
+                //url
+                type = "link";
+                const [linkpath, aliaspath] = parseLink(url).split("|");
+                link = linkpath;
+                alias = aliaspath;
+            } else {
+                type = "file";
+                const [linkpath, aliaspath] = parseLink(url).split("|");
+                alias = aliaspath && aliaspath.length ? aliaspath : null;
+                let file = this.app.metadataCache.getFirstLinkpathDest(
+                    linkpath,
+                    ""
+                );
+                if (!file) throw new Error();
+                link = this.app.vault.getResourcePath(file);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+        return { link, id: encodeURIComponent(url), alias };
     }
 }
